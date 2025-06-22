3eb10e3f5adae375dd992388e07f5e162b5dc3bc
Bharadwaja Cheruvu <cbharadwaja1@gmail.com>
2025-06-21 21:43:52 -0500

    Delete frontend directory

--- frontend/src/components/InsightDisplay.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const InsightDisplay: React.FC<{ insights: string }> = ({ insights }) => (
-  <div className="vds-alert vds-alert--info vds-padding-block-md">
-    <h2 className="vds-heading--medium vds-margin-block-end-sm">Insights</h2>
-    <p>{insights}</p>
-  </div>
-);
